89c5a567653e97eb15667ecaf84a970adc9b0db6
Arang0109 <kmsq321@gmail.com>
2025-11-17 14:33:44 +0900

    CardSearchConditionDto 생성

--- src/main/java/com/project/moru/domain/entity/card/Card.java
+++ src/main/java/com/project/moru/domain/entity/card/Card.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 @Table(name = "card")
 public class Card extends BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "card_id")
     private Long cardId;
 
--- src/main/java/com/project/moru/service/CardService.java
+++ src/main/java/com/project/moru/service/CardService.java
@@ -7,10 +7,10 @@ import com.project.moru.domain.entity.card.Card;
 import java.util.List;
 
 public interface CardService {
+    List<Card> findAll();
     Card findById(Long id);
-    void deleteCardById(Long id, Long userId);
     Card saveCard(CardCreateRequestDto cardCreateRequestDto, Long userId);
     Card modifyCard(Long id, CardUpdateRequestDto cardUpdateRequestDto, Long userId);
-    List<Card> findAll();
+    void deleteCardById(Long id, Long userId);
 
 }
