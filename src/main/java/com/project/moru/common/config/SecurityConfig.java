2339c433b5f05d283311e9460d626d591cb38648
최효선 <gytjs2998@naver.com>
2026-01-04 20:23:58 +0900

    refactor: SecurityConfig 덱 조회 설정

--- src/main/java/com/project/moru/common/config/SecurityConfig.java
+++ src/main/java/com/project/moru/common/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.project.moru.common.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfig {
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeRequests(auth -> auth
+            .antMatchers(HttpMethod.GET,"/api/card").permitAll()
             .antMatchers("/api/auth/register", "/api/auth/login", "/api/auth/refresh").permitAll()
             .antMatchers(
                 "/api/auth/**",
